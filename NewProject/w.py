d77371e65a9aec5cbb2a85fcd35255a1d98d83ba
Rafeh Hussain <hussar4@mcmaster.ca>
2020-03-23 13:35:28 -0400

    files

--- NewProject/w.py
+++ NewProject/w.py
@@ -1,13 +1,15 @@
 import random
 
-file = open("text.txt", "r")
+f = open("text.txt", "r")
+file = f.readlines()
+f.close()
 
 list = ['!','?','"',',','.',':',';','(',')','\n']
 vocab = []
 occurances = {}
 
 for line in file:
-	words = line.split(" ")
+	words = line.split(' ')
 	for word in words:
 		for char in list:
 			word = word.replace(char, '')
@@ -19,10 +21,19 @@ for line in file:
 		occurances[word] = occurances[word] + 1
 
 
-sentences = []
+sentence = ""
+sBreakers = ['.','?','!']
+midSentence = ['"',','(',')']
+sentences = [[],[],[]]
 
 for line in file:
-	sentence = line.split(".")
+	sentence += line
+
+
+for char in sBreakers:
+	sentence.split(char)
+
+print sentence
 	
 
 str = ""
@@ -30,4 +41,4 @@ for i in range(10):
 	x = random.randint(0,len(vocab)-1)
 	str += vocab[x]+" "
 
-print str
+
