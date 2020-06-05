1c77f2f81b0a0ab84eca1a0c0dd263fc51b9302b
bonako <47225187+bonako@users.noreply.github.com>
2020-06-04 23:17:02 -0400

    removed keys/apis

--- webApp/v2/map.js
+++ webApp/v2/map.js
@@ -1,11 +1,11 @@
   var firebaseConfig = {
-     apiKey: "AIzaSyDQ62BC1Gcg-tJJgl7rkqxuDO8x7cSP_08",
-     authDomain: "parkingrabbit-bf78a.firebaseapp.com",
-     databaseURL: "https://parkingrabbit-bf78a.firebaseio.com",
-     projectId: "parkingrabbit-bf78a",
-     storageBucket: "parkingrabbit-bf78a.appspot.com",
-     messagingSenderId: "94492827210",
-     appId: "1:94492827210:web:43ef5ed366dac05eb2d0b3"
+     apiKey: "",
+     authDomain: "",
+     databaseURL: "",
+     projectId: "",
+     storageBucket: "",
+     messagingSenderId: "",
+     appId: ""
    };
    // Initialize Firebase
    firebase.initializeApp(firebaseConfig);
