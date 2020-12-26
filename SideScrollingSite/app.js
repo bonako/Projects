3473f0f2d57b6770ddfa705b914cda3b996371ba
bonako <47225187+bonako@users.noreply.github.com>
2020-12-26 16:22:41 -0500

    Update app.js

--- SideScrollingSite/app.js
+++ SideScrollingSite/app.js
@@ -1,12 +1,12 @@
     // Your web app's Firebase configuration
     var firebaseConfig = {
-        apiKey: "AIzaSyDQ62BC1Gcg-tJJgl7rkqxuDO8x7cSP_08",
+        apiKey: "AIzaSyDQ62BC1Gcg-key",
         authDomain: "parkingrabbit-bf78a.firebaseapp.com",
         databaseURL: "https://parkingrabbit-bf78a.firebaseio.com",
         projectId: "parkingrabbit-bf78a",
         storageBucket: "parkingrabbit-bf78a.appspot.com",
-        messagingSenderId: "94492827210",
-        appId: "1:94492827210:web:43ef5ed366dac05eb2d0b3"
+        messagingSenderId: "key",
+        appId: "1:94492827210:web:key"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
